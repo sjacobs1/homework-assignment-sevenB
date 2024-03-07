f76424571dd2f04b102bc08a1296fe5baa80464c
sebastianj7 <135590017+sebastianj7@users.noreply.github.com>
2024-03-07 09:26:33 +0200

    counter added
    button added that updates counter

--- sevenB/sevenB/ViewController.swift
+++ sevenB/sevenB/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    
+    @IBOutlet weak var labelCount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        count+=1
+        labelCount.text = "\(count)"
+    }
+    
 }
 
